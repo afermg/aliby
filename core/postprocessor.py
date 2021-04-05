7172de26fd2550f2f97cf4771a0bef4246d621e2
Alán Muñoz <amuoz@ed.ac.uk>
2021-04-05 19:06:19 +0100

    restructure folder
    Former-commit-id: 37563b91d2e1e492434db21d1c75b61a6f2ad192

---
+++ core/postprocessor.py
@@ -0,0 +1,74 @@
+import pkg_resources
+from pathlib import Path, PosixPath
+from typing import Union, List, Dict
+from datetime import datetime
+
+from compress_pickle import load, dump
+
+from core.experiment import Experiment
+from core.cells import Cells
+from core.segment import Tiler
+from core.io.matlab import matObject
+from core.experiment import Experiment
+
+
+class PostProcessor:
+    '''
+    Base class to perform feature extraction.
+    :param parameters: Parameters class with channels, reduction functions and
+        extraction functions to use.
+    :param source: Origin of experiment, if int it is assumed from Omero, if str
+        or Path
+    '''
+    def __init__(self, parameters=None, source: Union[int,str,Path]=None):
+        # self.params = parameters
+        if source is not None:
+            if type(source) is int:
+                self.expt_id = source
+                self.load_expt(source, omero=True)
+            elif type(source) is str or PosixPath:
+                self.load_expt(source, omero=False)
+
+    @property
+    def channels(self):
+        if not hasattr(self, '_channels'):
+            if type(self.params.tree) is dict:
+                self._channels = tuple(self.params.tree.keys())
+
+        return self._channels
+
+    @property
+    def current_position(self):
+        assert hasattr(self, 'expt'), 'No experiment loaded.'
+
+        if hasattr(self, 'tiler'):
+            assert self.expt.current_position.name == self.tiler.current_position
+
+        return self.expt.current_position
+
+    def load_expt(self, source:Union[int,str], omero:bool=False) -> None:
+        if omero:
+            self.expt = Experiment.from_source(self.expt_id, #Experiment ID on OMERO
+                                'upload', #OMERO Username
+                                '***REMOVED***', #OMERO Password
+                                'islay.bio.ed.ac.uk', #OMERO host
+                                port=4064 #This is default
+                                )
+        else:
+            self.expt = Experiment.from_source(source)
+            self.expt_id = self.expt.exptID
+
+    def load_tiler_cells(self) -> None:
+        self.tiler = Tiler(self.expt)
+        self.cells = Cells()
+        self.cells = self.cells.from_source(
+            self.expt.current_position.annotation)
+
+    def load_extraction(self, folder=None)-> None:
+        if folder is None:
+            folder = Path(self.expt.name + '/extraction')
+        
+        self.extraction = {}
+        for pos in self.expt.positions:
+            self.extraction[pos] = load(folder / Path(pos + '.gz'))
+            
---
+++ examples/testing.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+
+from matplotlib import pyplot as plt
+from pandas import Series
+
+from postprocessor.core.postprocessor import PostProcessor
+from postprocessor.core.tracks import non_uniform_savgol
+
+pp = PostProcessor(source=19831)
+pp.load_tiler_cells()
+f = '/home/alan/Documents/libs/extraction/extraction/examples/gluStarv_2_0_x2_dual_phl_ura8_00/extraction'
+pp.load_extraction('/home/alan/Documents/libs/extraction/extraction/examples/' + pp.expt.name + '/extraction/')
+
+tmp=pp.extraction['phl_ura8_002']
+
+# prepare data
+test = tmp[('GFPFast', np.maximum, 'mean')]
+clean = test.loc[test.notna().sum(axis=1) > 30]
+
+window = 9
+degree = 3
+savgol_on_srs = lambda x: Series(non_uniform_savgol(x.dropna().index, x.dropna().values,
+                                                  window, degree), index=x.dropna().index)
+
+smooth = clean.apply(savgol_on_srs, axis=1)
+
+from random import randint
+
+x = randint(0, len(smooth))
+plt.plot(clean.iloc[x], 'b')
+plt.plot(smooth.iloc[x], 'r')
+plt.show()
+
+def growth_rate(data:Series, alg=None, filt = {'kind':'savgol','window':9, 'degree':3}):
+    if alg is None:
+        alg='standard'
+
+    if filt: #TODO add support for multiple algorithms
+        data = Series(non_uniform_savgol(data.dropna().index, data.dropna().values,
+                                         window, degree), index = data.dropna().index)
+
+        
+    
+    return Series(np.convolve(data,diff_kernel ,'same'), index=data.dropna().index)
+
+import numpy as np
+diff_kernel = np.array([1,-1])
+gr = clean.apply(growth_rate, axis=1)
+    
+
+def sort_df(df, by='first', rev=True):
+    nona = df.notna()
+    if by=='len':
+        idx = nona.sum(axis=1)
+    elif by=='first':
+        idx = nona.idxmax(axis=1)
+    idx = idx.sort_values().index
+
+    if rev:
+        idx = idx[::-1]
+
+    return df.loc[idx]
+
+test = tmp[('GFPFast', np.maximum, 'median')]
+test2 = tmp[('pHluorin405', np.maximum, 'median')]
+ph = test/test2
+ph = ph.stack().reset_index(1)
+ph.columns = ['tp', 'fl']
+
+def m2p5_med(ext, ch, red=np.maximum):
+    m2p5pc = ext[(ch, red, 'max2p5pc')]
+    med = ext[(ch, red, 'median')]
+
+    result = m2p5pc/med
+
+    return result
+
+def plot_avg(df):
+    df = df.stack().reset_index(1)
+    df.columns = ['tp', 'val']
+
+    sns.relplot(x=df['tp'],y=df['val'], kind='line')
+    plt.show()
