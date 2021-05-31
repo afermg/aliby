4f42962e93ea4feb79456eb0ff7b8bab911c104e
Alán Muñoz <amuoz@ed.ac.uk>
2021-05-31 21:57:27 +0100

    Add extraction to pipeline script

--- core/cells.py
+++ core/cells.py
@@ -31,94 +31,153 @@ class Cells:
         pass
 
     @staticmethod
-    def from_source(source, type=None):
+    def from_source(source: Union[PosixPath, str], kind: str = None):
         if isinstance(source, str):
             source = Path(source)
-            if type is None:
-                # Infer type from filename
-                type = 'matlab' if source.suffix == '.mat' else 'hdf5'
-        return cell_factory(source, type)
+        if kind is None:
+            # Infer kind from filename
+            kind = "matlab" if source.suffix == ".mat" else "hdf5"
+        return cell_factory(source, kind)
+
+    @staticmethod
+    def _asdense(array):
+        if not isdense(array):
+            array = array.todense()
+        return array
+
+    @staticmethod
+    def _astype(array, kind):
+        # Convert sparse arrays if needed and if kind is 'mask' it fills the outline
+        array = Cells._asdense(array)
+        if kind == "mask":
+            array = ndimage.binary_fill_holes(array).astype(int)
+        return array
 
 
-def is_or_in(item, arr):
-    if isinstance(arr, (list, np.ndarray)):
-        return item in arr
-    else:
-        return item == arr
+# def is_or_in(item, arr): #TODO CLEAN if not being used
+#     if isinstance(arr, (list, np.ndarray)):
+#         return item in arr
+#     else:
+#         return item == arr
+
+
+from core.io.hdf5 import hdf_dict
+
+
+from functools import wraps
 
 
 class CellsHDF(Cells):
-    # TODO implement cells information from HDF5 file format
+    # DONE implement cells information from HDF5 file format
     # TODO combine all the cells of one strain into a cellResults?
     # TODO filtering
     def __init__(self, file):
-        pass
+        self._file = file
+        self._info = hdf_dict(self._file.get("/cell_info"))
+
+    def __getitem__(self, item):
+        _item = "_" + item
+        if not hasattr(self, _item):
+            setattr(self, _item, self._info[item][()])
+        return getattr(self, _item)
+
+    def _get_idx(self, cell_id, trap_id):
+        return (self["cell_label"] == cell_id) & (self["trap"] == trap_id)
 
     def where(self, cell_id, trap_id):
-        pass
+        indices = self._get_idx(cell_id, trap_id)
+        return self["timepoints"][indices], indices
 
     def outline(self, cell_id, trap_id):
-        pass
+        times, indices = self.where(cell_id, trap_id)
+        return times, self["edgemasks"][indices]
 
     def mask(self, cell_id, trap_id):
-        pass
+        times, outlines = self.outline(cell_id, trap_id)
+        return times, np.array(
+            [ndimage.morphology.binary_fill_holes(o) for o in outlines]
+        )
+
+    def at_time(self, timepoint, kind="mask"):
+        self.traps = self["trap"]
+        self.edgemasks = self["edgemasks"]
+        tp_indices = self["timepoint"] == timepoint
+        trap_indices = [
+            self.edgemasks[(self.traps == trap_id) & tp_indices]
+            for trap_id in set(self.traps)
+        ]
+        return [
+            [self._astype(cells, kind) for cells in trap_id] for trap_id in trap_indices
+        ]
+
+    def split_by_trap_timepoint(self):
+        # Convert from hdf5 flat format to nested lists
+        tp_set = set(self["timepoint"])
+        trap_set = set(self["trap"])
+        cell_labels = [
+            [[] for _ in range(max(trap_set) + 1)] for _ in range(max(tp_set) + 1)
+        ]
+        for lbl, tp, trap in zip(self["cell_label"], self["timepoint"], self["trap"]):
+            cell_labels[tp][trap].append(lbl)  # TODO watch out for different timepoints
+        return cell_labels
+
+    def close(self):
+        self._file.close()
 
 
 class CellsMat(Cells):
     def __init__(self, mat_object):
         super(CellsMat, self).__init__()
         # TODO add __contains__ to the matObject
-        timelapse_traps = mat_object.get('timelapseTrapsOmero',
-                                            mat_object.get('timelapseTraps',
-                                                              None))
+        timelapse_traps = mat_object.get(
+            "timelapseTrapsOmero", mat_object.get("timelapseTraps", None)
+        )
         if timelapse_traps is None:
-            raise NotImplementedError("Could not find a timelapseTraps or "
-                                      "timelapseTrapsOmero object. Cells "
-                                      "from cellResults not implemented")
+            raise NotImplementedError(
+                "Could not find a timelapseTraps or "
+                "timelapseTrapsOmero object. Cells "
+                "from cellResults not implemented"
+            )
         else:
-            self.trap_info = timelapse_traps['cTimepoint']['trapInfo']
+            self.trap_info = timelapse_traps["cTimepoint"]["trapInfo"]
             if isinstance(self.trap_info, list):
-                self.trap_info = {k: list([res.get(k, [])
-                                           for res in self.trap_info])
-                                        for k in self.trap_info[0].keys()}
+                self.trap_info = {
+                    k: list([res.get(k, []) for res in self.trap_info])
+                    for k in self.trap_info[0].keys()
+                }
 
     def where(self, cell_id, trap_id):
-        times, indices = zip(*[(tp, np.where(cell_id == x)[0][0])
-                               for tp, x in
-                               enumerate(self.trap_info['cellLabel'][:,
-                                         trap_id].tolist())
-                               if np.any(cell_id == x)])
+        times, indices = zip(
+            *[
+                (tp, np.where(cell_id == x)[0][0])
+                for tp, x in enumerate(self.trap_info["cellLabel"][:, trap_id].tolist())
+                if np.any(cell_id == x)
+            ]
+        )
         return times, indices
 
     def outline(self, cell_id, trap_id):
         times, indices = self.where(cell_id, trap_id)
-        info = self.trap_info['cell'][times, trap_id]
+        info = self.trap_info["cell"][times, trap_id]
 
         def get_segmented(cell, index):
-            if cell['segmented'].ndim == 0:
-                return cell['segmented'][()].todense()
+            if cell["segmented"].ndim == 0:
+                return cell["segmented"][()].todense()
             else:
-                return cell['segmented'][index].todense()
+                return cell["segmented"][index].todense()
 
-        segmentation_outline = [get_segmented(cell, idx)
-                                for idx, cell in zip(indices, info)]
+        segmentation_outline = [
+            get_segmented(cell, idx) for idx, cell in zip(indices, info)
+        ]
         return times, np.array(segmentation_outline)
 
     def mask(self, cell_id, trap_id):
         times, outlines = self.outline(cell_id, trap_id)
-        return times, np.array([ndimage.morphology.binary_fill_holes(o) for
-                                o in outlines])
+        return times, np.array(
+            [ndimage.morphology.binary_fill_holes(o) for o in outlines]
+        )
 
-    def _astype(self, array, type):
-        if type == 'outline':
-            return np.array(array.todense())
-        elif type == 'mask':
-            arr = np.array(array.todense())
-            return ndimage.binary_fill_holes(arr).astype(int)
-        else:
-            return array
-
-    def at_time(self, timepoint, type='outline'):
+    def at_time(self, timepoint, kind="outline"):
         """Returns the segmentations for all the cells at a given timepoint.
 
         FIXME: this is extremely hacky and accounts for differently saved
