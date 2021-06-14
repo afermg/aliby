cff4a12ef57783f93d7abe5a9b0327488cce1caf
Alan Munoz <afer.mg@gmail.com>
2021-06-14 10:53:06 +0100

    add test hdf file
    Former-commit-id: a0a983dec8f4e1468eab1cc65ad3d617f2470917

---
+++ core/functions/test_hdf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# c=CellsHDF.from_source("/home/alan/Documents/sync_docs/PhD/tmp/DO6MS2_003store.h5")
+import h5py
+from core.cells import Cells
+import pandas as pd
+
+f = h5py.File("/home/alan/Documents/sync_docs/PhD/tmp/DO6MS2_003store.h5")
+tracks = f["/extraction/general/None/area"][()]
+cell = Cells.from_source("/home/alan/Documents/sync_docs/PhD/tmp/DO6MS2_003store.h5")
+from postprocessor.core.picker import Picker
+
+picker = Picker(tracks=pd.DataFrame(tracks), cells=cell)
--- core/processor.py
+++ core/processor.py
@@ -1,13 +1,41 @@
+class Parameters:
+    def __init__(self, merger=None, picker=None, processes=None, branches=None):
+        self.merger = merger
+        self.picker = picker
+        self.processes = processes
+        self.branches = branches
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+
 class PostProcessor:
-    def __init__(self, parameters):
+    def __init__(self, parameters, signals):
         self.parameters = parameters
 
         self.merger = Merger(parameters["merger"])
         self.picker = Picker(parameters["picker"])
-        self.processes, self.branches = [
-            (self.get_process(process), branch)
-            for process, branch in parameters["processes"]
+        self.processes = [
+            self.get_process(process) for process in parameters["processes"]
         ]
+        self.branches = parameters["branches"]
 
     def run(self):
+        self.merger.run(signals.get_branch(self.branches["merger"]))
+        self.picker.run(signals.get_branch(self.branches["picker"]))
+        for process, branch in zip(self.processes, self.branches["processes"]):
+            process.run(signals.get_branch(branch))
+
+
+class Signals:
+    def __init__(self, file):
+        self._file = h5py.File(file, "r")
+
+    def get_branch(self, branch):
+        return self._file[branch][()]
+
+    def branch_to_df(self):
         pass
+
+    def close(self):
+        self._file.close()
