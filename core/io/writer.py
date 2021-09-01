c661719eab35eb16775d2698c76db8180e218ded
Alán Muñoz <amuoz@ed.ac.uk>
2021-09-01 20:09:46 +0100

    add dynamic datasets

--- core/io/writer.py
+++ core/io/writer.py
@@ -1,12 +1,13 @@
-from collections.abc import Iterable
-from typing import Union, List, Dict
-from itertools import accumulate
-
 import h5py
 import numpy as np
 import pandas as pd
+from collections.abc import Iterable
+from typing import Dict
+
+from utils_find_1st import find_1st, cmp_equal
 
 from core.io.base import BridgeH5
+
 
 #################### Dynamic version ##################################
 
@@ -141,23 +142,39 @@ class Writer(BridgeH5):
             self.compression = "gzip"
 
     def write(
-        self, path: str, data: Iterable = None, meta: Dict = {}, overwrite: bool = True
+        self,
+        path: str,
+        data: Iterable = None,
+        meta: Dict = {},
+        overwrite: str = "minimal",
     ):
+        """
+        Parameters
+        ----------
+        path : str
+            Path inside h5 file to write into.
+        data : Iterable, default = None
+        meta : Dict, default = {}
+
+
+        """
         with h5py.File(self.filename, "a") as f:
-            over_txt = ""
-            if overwrite:
-                over_txt = "over"
+            if overwrite == "overwrite":
                 self.deldset(f, path)
-            else:  # Add a number if needed
+            elif overwrite == "accumulate":  # Add a number if needed
                 if path in f:
                     parent, name = path.rsplit("/", maxsplit=1)
                     n = sum([x.startswith(name) for x in f[path]])
                     path = path + str(n).zfill(3)
+            elif overwrite == "skip":
+                if path in f:
+                    print("Skipping dataset {}".format(path))
+                    return None
 
             # f.create_group(path)
             print(
-                "{}writing {} to {} and {} metadata fields".format(
-                    over_txt, type(data), path, len(meta)
+                "{} {} to {} and {} metadata fields".format(
+                    overwrite, type(data), path, len(meta)
                 )
             )
             if data is not None:
@@ -166,6 +183,7 @@ class Writer(BridgeH5):
                 for attr, metadata in meta.items():
                     self.write_meta(f, path, attr, data=metadata)
 
+    # def write_dset(self, f: h5py.File, path: str, data: Iterable, overwrite: str):
     def write_dset(self, f: h5py.File, path: str, data: Iterable):
         if isinstance(data, pd.DataFrame):
             self.write_df(f, path, data, compression=self.compression)
@@ -220,38 +238,148 @@ class Writer(BridgeH5):
             f.create_dataset(
                 name=id_path,
                 shape=ids.shape,
-                dtype=df.dtypes[0],
+                dtype=pd_index.dtypes.iloc[0],
                 compression=kwargs.get("compression", None),
             )
             indices = f[id_path]
             indices[()] = ids
 
-    @staticmethod
-    def write_df(f, path, df, **kwargs):
-        f.require_group(path)  # TODO check if we can remove this
-
+    def write_df(self, f, path, df, **kwargs):
         values_path = path + "/values"
-        f.create_dataset(
-            name=values_path,
-            shape=df.shape,
-            chunks=(min(df.shape[0], 1), df.shape[1]),
-            dtype=df.dtypes.iloc[0],
-            compression=kwargs.get("compression", None),
-        )
-        dset = f[values_path]
-        dset[()] = df.values
+        if path not in f:
+            max_ncells = 2e5
+            max_tps = 1000
+            f.create_dataset(
+                name=values_path,
+                shape=df.shape,
+                # chunks=(min(df.shape[0], 1), df.shape[1]),
+                dtype=df.dtypes.iloc[0],
+                maxshape=(max_ncells, max_tps),
+                compression=kwargs.get("compression", None),
+            )
+            dset = f[values_path]
+            dset[()] = df.values
 
-        for name in df.index.names:
             dtype = "uint16"  # if name != "position" else "str"
-            indices_path = path + "/" + name
-            f.create_dataset(name=indices_path, shape=(len(df),), dtype=dtype)
-            dset = f[indices_path]
-            dset[()] = df.index.get_level_values(level=name).tolist()
+            for name in df.index.names:
+                indices_path = "/".join((path, name))
+                f.create_dataset(
+                    name=indices_path,
+                    shape=(len(df),),
+                    dtype=dtype,
+                    chunks=True,
+                    maxshape=(max_ncells,),
+                )
+                dset = f[indices_path]
+                dset[()] = df.index.get_level_values(level=name).tolist()
 
-        tp_path = path + "/timepoint"
-        f.create_dataset(
-            name=tp_path,
-            shape=(df.shape[1],),
-            dtype="uint16",
+            tp_path = path + "/timepoint"
+            f.create_dataset(
+                name=tp_path,
+                shape=(df.shape[1],),
+                maxshape=(max_tps,),
+                dtype="uint16",
+            )
+            tps = df.columns.tolist()
+            f[tp_path][tps] = tps
+
+        # dset =  f.require_group(path)  # TODO check if we can remove this
+
+        else:
+            n_newcells = 0
+            # if len(df.index.names) > 1:
+            dset = f[values_path]
+            # if not hasattr(self, "new_ids"):
+            existing_ids = self.get_existing_ids(
+                f, [path + "/" + x for x in df.index.names]
+            )
+            # Split indices in existing and new
+            existing_multis, new_multis = self.find_ids(
+                existing=existing_ids,
+                new=df.index.tolist(),
+            )
+            self.existing_indices = np.array(
+                locate_indices(existing_ids, existing_multis)
+            )
+            incremental_existing = np.argsort(self.existing_indices)
+            self.existing_indices = self.existing_indices[incremental_existing]
+            self.existing_multi = existing_multis[incremental_existing]
+
+            self.new_indices = list(locate_indices(existing_ids, new_multis))
+            self.new_multi = new_multis
+
+            existing_values = df.loc[
+                [tuple_or_int(x) for x in self.existing_multi]
+            ].values
+            new_values = df.loc[[tuple_or_int(x) for x in self.new_multi]].values
+            ncells, ntps = f[values_path].shape
+
+            # Add found cells
+            dset.resize(dset.shape[1] + df.shape[1], axis=1)
+            dset[:, ntps:] = np.nan
+            for i, tp in enumerate(df.columns):
+                dset[self.existing_indices, tp] = existing_values[:, i]
+            # Add new cells
+            n_newcells = len(self.new_indices)
+            dset.resize(dset.shape[0] + n_newcells, axis=0)
+            dset[ncells:, :] = np.nan
+
+            for i, tp in enumerate(df.columns):
+                dset[ncells:, tp] = new_values[:, i]
+
+            # save indices
+            for i, name in enumerate(df.index.names):
+                tmp = path + "/" + name
+                dset = f[tmp]
+                n = dset.shape[0]
+                dset.resize(n + n_newcells, axis=0)
+                dset[n:] = (
+                    self.new_multi[:, i]
+                    if len(self.new_multi.shape) > 1
+                    else self.new_multi
+                )
+
+            tmp = path + "/timepoint"
+            dset = f[tmp]
+            n = dset.shape[0]
+            dset.resize(n + len(df.columns), axis=0)
+            if path.endswith("imBackground"):
+                print("stop")
+            dset[n:] = df.columns.tolist()
+
+    @staticmethod
+    def get_existing_ids(f, paths):
+        return np.array([f[path][()] for path in paths]).T
+
+    @staticmethod
+    def find_ids(existing, new):
+        set_existing = set([tuple(*x) for x in zip(existing.tolist())])
+        existing_cells = np.array(list(set_existing.intersection(new)))
+        new_cells = np.array(list(set(new).difference(set_existing)))
+        # for i in range(new_cells.shape[1] - 1, -1, -1):
+        #     new_cells = new_cells[new_cells[:, i].argsort(kind="mergesort")]
+
+        return (
+            existing_cells,
+            new_cells,
         )
-        f[tp_path][()] = df.columns.tolist()
+
+
+# @staticmethod
+def locate_indices(existing, new):
+    if len(new.shape) > 1:
+        return [
+            find_1st(
+                (existing[:, 0] == n[0]) & (existing[:, 1] == n[1]), True, cmp_equal
+            )
+            for n in new
+        ]
+    else:
+        return [find_1st(existing[:, 0] == n, True, cmp_equal) for n in new]
+
+
+def tuple_or_int(x):
+    if isinstance(x, Iterable):
+        return tuple(x)
+    else:
+        return x
