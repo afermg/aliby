2b42f2aec5845f088e571c413888f92a7b369318
Alan Munoz <alan.munoz@ed.ac.uk>
2021-09-30 20:33:39 +0100

    update setup

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'omero-py>=5.6.2',
         'zeroc-ice==3.6.5',
         'tensorflow>=1.14,<=2.3'
-        'baby@git+ssh://git@git.ecdf.ed.ac.uk/jpietsch/baby@dev',
+        'baby@git+ssh://git@git.ecdf.ed.ac.uk/jpietsch/baby.git#dev',
         'logfile_parser@git+ssh://git@git.ecdf.ed.ac.uk/swain-lab/python-pipeline/logfile_parser.git',
         ],
 )
