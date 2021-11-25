6c6b629d127f875ec1457e0164df7b2c22ecb767
Alán Muñoz <amuoz@ed.ac.uk>
2021-11-25 16:19:49 +0000

    add catch22
    Former-commit-id: 5994fc649078754c45bf497d0425eaf18cffacdc

---
+++ core/processes/catch22.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+
+import numpy as np
+import pandas as pd
+from sklearn import decomposition
+from catch22 import catch22_all
+
+from agora.base import ParametersABC, ProcessABC
+
+
+class catch22Parameters(ParametersABC):
+    """
+    Parameters
+        :min_len: Prefilter to account only for long-signal cells
+        :n_components: Number of components to use
+    """
+
+    def __init__(self, min_len, n_components):
+        super().__init__()
+
+    @classmethod
+    def default(cls):
+        return cls.from_dict({})
+
+
+class catch22(ProcessABC):
+    """
+    catch22 class. It produces 22 normalised features for each time lapse in the signal (using the catch22 library.)
+    """
+
+    def __init__(self, parameters: catch22Parameters):
+        super().__init__(parameters)
+
+    def run(self, signal: pd.DataFrame):
+        thresh = (
+            self.min_len
+            if isinstance(self.min_len, int)
+            else signal.shape[1] * self.min_len
+        )
+        signal = signal.loc[signal.notna().sum(axis=1) > thresh]
+        catches = [catch22_all(adf.iloc[i, :].dropna().values) for i in range(len(adf))]
+
+        norm = pd.DataFrame(catches, index=signal.index, columns=catches[0]["names"])
+
+        return norm
+
+    # pca = decomposition.PCA(n_components=self.n_components)
+    # pca.fit(norm)
+    # Y = pca.transform(norm)
+    # return pd.DataFrame(Y, index=norm.index)
