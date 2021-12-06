e586a5265b3a4eed99fadbf8d7045c351b88986c
Alán Muñoz <amuoz@ed.ac.uk>
2021-12-06 12:47:30 +0000

    update births to generate timelapse
    Former-commit-id: 05e408b95222a82fe68cda4b656ec091a34d1067

--- core/processes/births.py
+++ core/processes/births.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import pandas as pd
 
-from agora.io.base import ParametersABC, ProcessABC
+from agora.base import ParametersABC, ProcessABC
 
 
 class birthsParameters(ParametersABC):
@@ -20,11 +21,30 @@ class birthsParameters(ParametersABC):
 
 class births(ProcessABC):
     """
-    Calculate the change in a signal depending on a window
+    Calculate births in a trap assuming one mother per trap
+
+    returns a pandas series with the births
     """
 
     def __init__(self, parameters: birthsParameters):
         super().__init__(parameters)
 
     def run(self, signal: pd.DataFrame):
-        pass
+        # vals = signal.groupby("trap").apply(lambda x: len(x) - 1)
+        birth_events = signal.groupby("trap").apply(lambda x: x.first_valid_index())
+        fvi = signal.apply(lambda x: x.first_valid_index(), axis=1)
+        fvi_list = fvi.groupby("trap").apply(list)
+
+        mothers = signal.groupby("trap").apply(
+            lambda x: x.index[x.notna().sum(axis=1).argmax()]
+        )
+
+        births = pd.DataFrame(
+            np.zeros((mothers.shape[0], signal.shape[1])).astype(bool),
+            index=mothers.index,
+            columns=signal.columns,
+        )
+        for i, v in fvi_list.items():
+            births.loc[i, v] = True
+
+        return births
