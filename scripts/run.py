99e0ab261e18947d040b3fd9603e8c7e904f031b
Alán Muñoz <amuoz@ed.ac.uk>
2021-12-17 11:41:03 +0000

    rename import in run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from pcore.pipeline import PipelineParameters, Pipeline
+from aliby.pipeline import PipelineParameters, Pipeline
 
 params = PipelineParameters.default(general={"expt_id": 19995, "distributed": 5})
 p = Pipeline(params)
