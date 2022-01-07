7d1e113768a206feeb0b8a6603184b2e73a468ce
Alán Muñoz <amuoz@ed.ac.uk>
2022-01-07 19:35:28 +0000

    add deps for testing cv2 and test bugfixes

--- tests/aliby/test_baby_client.py
+++ tests/aliby/test_baby_client.py
@@ -2,51 +2,53 @@ import json
 import time
 import numpy as np
 
-from core.experiment import ExperimentLocal
-from core.segment import Tiler
-from core.baby_client import BabyClient
+from aliby.experiment import ExperimentLocal
+from aliby.baby_client import BabyClient
+from tile.tiler import Tiler
 
-root_dir = '/Users/s1893247/PhD/pipeline-core/data/glclvl_0' \
-           '.1_mig1_msn2_maf1_sfp1_dot6_03'
+root_dir = (
+    "/Users/s1893247/PhD/pipeline-core/data/glclvl_0" ".1_mig1_msn2_maf1_sfp1_dot6_03"
+)
 
 expt = ExperimentLocal(root_dir, finished=True)
 seg_expt = Tiler(expt, finished=True)
 
 print(seg_expt.positions)
-seg_expt.current_position = 'pos007'
+seg_expt.current_position = "pos007"
 
-config = {"camera": "evolve",
-          "channel": "brightfield",
-          "zoom": "60x",
-          "n_stacks": "5z"}
+config = {"camera": "evolve", "channel": "brightfield", "zoom": "60x", "n_stacks": "5z"}
 
 baby_client = BabyClient(expt, **config)
 
-print("The session is {}".format(baby_client.sessions['default']))
+print("The session is {}".format(baby_client.sessions["default"]))
 
 # Channel 0, 0, X,Y,Z all
 num_timepoints = 5
 
-traps_tps = [seg_expt.get_traps_timepoint(tp, tile_size=81, channels=[0],
-                                         z=[0, 1, 2, 3, 4]).squeeze()
-            for tp in range(num_timepoints)]
+traps_tps = [
+    seg_expt.get_traps_timepoint(
+        tp, tile_size=81, channels=[0], z=[0, 1, 2, 3, 4]
+    ).squeeze()
+    for tp in range(num_timepoints)
+]
 
 segmentations = []
 try:
     for i, timpoint in enumerate(traps_tps):
         print("Sending timepoint {};".format(i))
-        status = baby_client.queue_image(timpoint,
-                                         baby_client.sessions['default'],
-                                         assign_mothers=True,
-                                         return_baprobs=True,
-                                         with_edgemasks=True)
+        status = baby_client.queue_image(
+            timpoint,
+            baby_client.sessions["default"],
+            assign_mothers=True,
+            return_baprobs=True,
+            with_edgemasks=True,
+        )
         while True:
             try:
-                print('Loading.', end='')
-                result = baby_client.get_segmentation(baby_client.sessions[
-                                                          'default'])
+                print("Loading.", end="")
+                result = baby_client.get_segmentation(baby_client.sessions["default"])
             except:
-                print('.', end='')
+                print(".", end="")
                 time.sleep(1)
                 continue
             break
@@ -56,10 +58,10 @@ except Exception as e:
     print(segmentations)
     raise e
 
-with open('segmentations.json', 'w') as fd:
+with open("segmentations.json", "w") as fd:
     json.dump(segmentations, fd)
 
-print('Done.')
+print("Done.")
 # print(len(segmentations[0]))
 # for i in range(5):
 #     print("trap {}".format(i))
--- tests/aliby/test_local_expt.py
+++ tests/aliby/test_local_expt.py
@@ -3,33 +3,33 @@ from logging.handlers import RotatingFileHandler
 import unittest
 from pathlib import Path
 
-from core.experiment import Experiment
+from aliby.experiment import Experiment
 
 ## LOGGING
-logger = logging.getLogger('core')
+logger = logging.getLogger("core")
 logger.handlers = []
 logger.setLevel(logging.DEBUG)
 
 console = logging.StreamHandler()
 console.setLevel(logging.WARNING)
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s")
 console.setFormatter(formatter)
 logger.addHandler(console)
 
-file_handler = RotatingFileHandler(filename='test.log',
-                                   maxBytes=1e5,
-                                   backupCount=1)
+file_handler = RotatingFileHandler(filename="test.log", maxBytes=1e5, backupCount=1)
 
 file_handler.setLevel(logging.DEBUG)
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s '
-                                   '- %(message)s')
+file_formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s " "- %(message)s"
+)
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
-logger.debug('Set up the loggers as test.')
+logger.debug("Set up the loggers as test.")
 
-data_directory = Path(__file__).parent.parent / 'data/'
-root_directory = data_directory / 'glclvl_0.1_mig1_msn2_maf1_sfp1_dot6_03'
+data_directory = Path(__file__).parent.parent / "data/"
+root_directory = data_directory / "glclvl_0.1_mig1_msn2_maf1_sfp1_dot6_03"
+
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -44,14 +44,11 @@ class TestCase(unittest.TestCase):
         self.assertTupleEqual(test_slice.shape, (2, 3, 100, 100, 3))
 
     def test_experiment_metadata(self):
-        self.assertTrue('positions' in self.expt.metadata.keys())
-        self.assertTrue('channels' in self.expt.metadata.keys())
-        self.assertTrue('time_settings' in self.expt.metadata.keys())
-        self.assertTrue('zsectioning' in self.expt.metadata.keys())
-
+        self.assertTrue("positions" in self.expt.metadata.keys())
+        self.assertTrue("channels" in self.expt.metadata.keys())
+        self.assertTrue("time_settings" in self.expt.metadata.keys())
+        self.assertTrue("zsectioning" in self.expt.metadata.keys())
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
