758215847cc8a6dfd6eaf5c2971f5357e5677285
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2022-05-11 13:32:46 +0100

    Add documentation for crosscorr process

--- core/multisignal/crosscorr.py
+++ core/multisignal/crosscorr.py
@@ -6,8 +6,6 @@ import pandas as pd
 from agora.abc import ParametersABC
 from postprocessor.core.abc import PostProcessABC
 
-# TODO: Add documentation
-
 
 class crosscorrParameters(ParametersABC):
     """
@@ -15,6 +13,9 @@ class crosscorrParameters(ParametersABC):
 
     Attributes
     ----------
+    t: int
+        Lag, in time points
+        FIXME: clarify with Peter.
     """
 
     _defaults = {
@@ -25,11 +26,34 @@ class crosscorrParameters(ParametersABC):
 class crosscorr(PostProcessABC):
     """ """
 
-    def __init__(self, parameters: alignParameters):
+    def __init__(self, parameters: crosscorrParameters):
         super().__init__(parameters)
 
     # TODO: make variable names more informative
     def run(self, yA: pd.DataFrame, yB: pd.DataFrame = None):
+        """Calculates normalised cross-correlations as a function of time.
+
+        Calculates normalised auto- or cross-correlations as a function of time.
+        Normalisation is by the product of the standard deviation for each
+        variable calculated across replicates at each time point.
+        With zero lag, the normalised correlation should be one.
+
+        Parameters
+        ----------
+        yA: array or aliby dataframe
+            An array of signal values, with each row a replicate measurement
+            and each column a time point.
+        yB: array or aliby dataframe (required for cross-correlation only)
+            An array of signal values, with each row a replicate measurement
+            and each column a time point.
+
+        Returns
+        -------
+        norm_corr: array or aliby dataframe
+            An array of the correlations with each row the result for the
+            corresponding replicate and each column a time point
+        """
+
         exampledf = yA.copy() if type(yA) == pd.core.frame.DataFrame else None
         # convert from aliby dataframe to arrays
         yA = yA.to_numpy()
