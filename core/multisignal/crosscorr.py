0f90ea8badf606a8762de3611080ad1c1fa784bf
Arin Wongprommoon <arin.wongprommoon@ed.ac.uk>
2022-05-11 14:32:04 +0100

    Make variable names more informative

--- core/multisignal/crosscorr.py
+++ core/multisignal/crosscorr.py
@@ -19,7 +19,7 @@ class crosscorrParameters(ParametersABC):
     """
 
     _defaults = {
-        "t": None,
+        "lagtime": None,
     }
 
 
@@ -29,8 +29,7 @@ class crosscorr(PostProcessABC):
     def __init__(self, parameters: crosscorrParameters):
         super().__init__(parameters)
 
-    # TODO: make variable names more informative
-    def run(self, yA: pd.DataFrame, yB: pd.DataFrame = None):
+    def run(self, trace_dfA: pd.DataFrame, trace_dfB: pd.DataFrame = None):
         """Calculates normalised cross-correlations as a function of time.
 
         Calculates normalised auto- or cross-correlations as a function of time.
@@ -40,10 +39,10 @@ class crosscorr(PostProcessABC):
 
         Parameters
         ----------
-        yA: array or aliby dataframe
+        trace_dfA: dataframe
             An array of signal values, with each row a replicate measurement
             and each column a time point.
-        yB: array or aliby dataframe (required for cross-correlation only)
+        trace_dfB: dataframe (required for cross-correlation only)
             An array of signal values, with each row a replicate measurement
             and each column a time point.
 
@@ -54,30 +53,33 @@ class crosscorr(PostProcessABC):
             corresponding replicate and each column a time point
         """
 
-        exampledf = yA.copy() if type(yA) == pd.core.frame.DataFrame else None
+        df = trace_dfA.copy() if type(trace_dfA) == pd.core.frame.DataFrame else None
         # convert from aliby dataframe to arrays
-        yA = yA.to_numpy()
+        trace_A = trace_dfA.to_numpy()
         # number of time points
-        n = yA.shape[1]
+        n_tps = trace_A.shape[1]
         # deviation from mean at each time point
-        dyA = yA - np.nanmean(yA, axis=0).reshape((1, n))
+        dmean_A = trace_A - np.nanmean(trace_A, axis=0).reshape((1, n_tps))
         # standard deviation at each time point
-        stdA = np.sqrt(np.nanmean(dyA ** 2, axis=0).reshape((1, n)))
-        if yB is not None:
-            yB = yB.to_numpy()
+        stdA = np.sqrt(np.nanmean(dmean_A ** 2, axis=0).reshape((1, n_tps)))
+        if trace_dfB is not None:
+            trace_B = trace_dfB.to_numpy()
             # cross correlation
-            dyB = yB - np.nanmean(yB, axis=0).reshape((1, n))
-            stdB = np.sqrt(np.nanmean(dyB ** 2, axis=0).reshape((1, n)))
+            dmean_B = trace_B - np.nanmean(trace_B, axis=0).reshape((1, n_tps))
+            stdB = np.sqrt(np.nanmean(dmean_B ** 2, axis=0).reshape((1, n_tps)))
         else:
             # auto correlation
-            dyB = dyA
+            dmean_B = dmean_A
             stdB = stdA
         # calculate correlation
-        corr = np.nan * np.ones(yA.shape)
+        corr = np.nan * np.ones(trace_A.shape)
         # lag r runs over time points
-        for r in np.arange(0, n):
-            prods = [dyA[:, self.t] * dyB[:, self.t + r] for self.t in range(n - r)]
-            corr[:, r] = np.nansum(prods, axis=0) / (n - r)
+        for r in np.arange(0, n_tps):
+            prods = [
+                dmean_A[:, self.lagtime] * dmean_B[:, self.lagtime + r]
+                for self.lagtime in range(n_tps - r)
+            ]
+            corr[:, r] = np.nansum(prods, axis=0) / (n_tps - r)
         norm_corr = np.array(corr) / stdA / stdB
-        # return as a df if yA is a df else as an array
-        return pd.DataFrame(norm_corr, index=exampledf.index, columns=exampledf.columns)
+        # return as a df if trace_dfA is a df else as an array
+        return pd.DataFrame(norm_corr, index=df.index, columns=df.columns)
