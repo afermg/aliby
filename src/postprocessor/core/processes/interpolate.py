5f5e198fb765e0d101573a51409e4cdb4b9de486
Alán Muñoz <alan.munoz@ed.ac.uk>
2022-10-05 19:01:00 +0100

    feat(process): add interpolate

---
+++ src/postprocessor/core/processes/interpolate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env jupyter
+
+import pandas as pd
+from agora.abc import ParametersABC
+
+from postprocessor.core.abc import PostProcessABC
+
+
+class interpolateParameters(ParametersABC):
+    """
+    Parameters
+    """
+
+    _defaults = {"limit_area": "inside"}
+
+
+class interpolate(PostProcessABC):
+    """
+    Interpolate process.
+    """
+
+    def __init__(self, parameters: interpolateParameters):
+        super().__init__(parameters)
+
+    def run(self, signal: pd.DataFrame):
+        return signal.interpolate(limit_area="inside", axis=1)
