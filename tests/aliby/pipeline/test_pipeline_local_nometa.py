dfccfc289399e89c29c4a1b194f2fad98e462891
Alán Muñoz <alan.munoz@ed.ac.uk>
2023-01-08 01:21:28 +0000

    test(pipeline): outcommen test content

--- tests/aliby/pipeline/test_pipeline_local_nometa.py
+++ tests/aliby/pipeline/test_pipeline_local_nometa.py
@@ -4,16 +4,15 @@ from aliby.pipeline import PipelineParameters, Pipeline
 
 
 def test_local_pipeline(file: str):
-    print(file)
-    # params = PipelineParameters.default(
-    #     general={
-    #         "expt_id": file,
-    #         "distributed": 0,
-    #         "directory": "../local/",
-    #         "overwrite": True,
-    #     },
-    #     tiler={"ref_channel": 0},
-    # )
-    # p = Pipeline(params)
+    params = PipelineParameters.default(
+        general={
+            "expt_id": file,
+            "distributed": 0,
+            "directory": "test_output/",
+            "overwrite": True,
+        },
+        tiler={"ref_channel": 0},
+    )
+    p = Pipeline(params)
 
-    # p.run()
+    p.run()
