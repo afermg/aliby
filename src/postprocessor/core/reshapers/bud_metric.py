759517535a582a9cfd0659ff4c8e6a7705dd8bf1
Alán Muñoz <alan.munoz@ed.ac.uk>
2023-04-03 13:50:01 +0100

    change(bud_metric): incorporate mother_label level

--- src/postprocessor/core/reshapers/bud_metric.py
+++ src/postprocessor/core/reshapers/bud_metric.py
@@ -1,9 +1,10 @@
+import typing as t
 from typing import Dict, Tuple
 
 import numpy as np
 import pandas as pd
-from agora.utils.lineage import mb_array_to_dict
 
+from agora.utils.lineage import mb_array_to_dict
 from postprocessor.core.lineageprocess import (
     LineageProcess,
     LineageProcessParameters,
@@ -30,15 +31,21 @@ class BudMetric(LineageProcess):
     def run(
         self,
         signal: pd.DataFrame,
-        mother_bud_ids: Dict[pd.Index, Tuple[pd.Index]] = None,
+        lineage: Dict[pd.Index, Tuple[pd.Index]] = None,
     ):
-        if mother_bud_ids is None:
-            mother_bud_ids = mb_array_to_dict(self.lineage)
+        if lineage is None:
+            if hasattr(self, "lineage"):
+                lineage = self.lineage
+            else:
+                assert "mother_label" in signal.index.names
+                lineage = signal.index.to_list()
 
-        return self.get_bud_metric(signal, mother_bud_ids)
+        return self.get_bud_metric(signal, mb_array_to_dict(lineage))
 
     @staticmethod
-    def get_bud_metric(signal: pd.DataFrame, md: Dict[Tuple, Tuple[Tuple]]):
+    def get_bud_metric(
+        signal: pd.DataFrame, md: Dict[Tuple, Tuple[Tuple]] = None
+    ):
         """
 
         signal: Daughter-inclusive dataframe
@@ -56,38 +63,47 @@ class BudMetric(LineageProcess):
 
         """
         mothers_mat = np.zeros((len(md), signal.shape[1]))
-        flag = 0  # Flag determines if mothers (1), daughters (2) or both were missing (3)
+        cells_were_dropped = 0  # Flag determines if mothers (1), daughters (2) or both were missing (3)
 
-        md_ = {k: md[k] for k in signal.index.intersection(md.keys())}
-        if len(md_) < len(md):
-            flag += 1
+        md_index = signal.index.droplevel("mother_label")
+        if md is not None:  # Get intersection of lineage and current index
+            related_items = set(
+                [*md.keys(), *[y for x in md.values() for y in x]]
+            )
+            intersection = md_index.intersection(related_items)
+            if len(intersection) < len(signal):
+                print("Dropped cells before bud_metric")  # TODO log
 
-        dau_flags = False
-        for i, daughters in enumerate(md.values()):
-            if len(daughters_inter := signal.index.intersection(daughters)):
-
-                dau_vals = signal.loc[set(daughters_inter)].droplevel("trap")
-                sorted_da_ids = dau_vals.sort_index(level="cell_label")
-                tp_fvt = sorted_da_ids.apply(
-                    lambda x: x.last_valid_index(), axis=0
-                )
-
-                tp_fvt = sorted_da_ids.index.get_indexer(tp_fvt)
-                tp_fvt[tp_fvt < 0] = sorted_da_ids.shape[0] - 1
-
-                buds_metric = np.choose(tp_fvt, sorted_da_ids.values)
-                # mothers_mat[i, tp_fvt[0] : tp_fvt[0] + len(buds_metric)] = buds_metric
-                mothers_mat[i] = buds_metric
-
-        if dau_flags:
-            flag += 2
-
-        if flag:
-            print(
-                f"BudMetric: Cell labels of the kind {('mother','daughter','both')[flag]} were dropped"
+            signal = (
+                signal.reset_index("mother_label")
+                .loc(axis=0)[intersection]
+                .set_index("mother_label", append=True)
             )
 
-        df = pd.DataFrame(mothers_mat, index=md.keys(), columns=signal.columns)
-        if len(df):
-            df.index.names = signal.index.names
-        return df
+        names = list(signal.index.names)
+        del names[-2]
+        output_df = (
+            signal.loc[signal.index.get_level_values("mother_label") > 0]
+            .groupby(names)
+            .apply(lambda x: _combine_daughter_tracks(x))
+        )
+        output_df["padding_level"] = 0
+        output_df.set_index("padding_level", append=True, inplace=True)
+
+        if len(output_df):
+            output_df.index.names = signal.index.names
+        return output_df
+
+
+def _combine_daughter_tracks(tracks: t.Collection[pd.Series]):
+    """
+    Combine multiple time series of cells into one, overwriting values
+    prioritising the most recent entity.
+    """
+    sorted_da_ids = tracks.sort_index(level="cell_label")
+    tp_fvt = sorted_da_ids.apply(lambda x: x.last_valid_index(), axis=0)
+    tp_fvt = sorted_da_ids.index.get_indexer(tp_fvt)
+    tp_fvt[tp_fvt < 0] = sorted_da_ids.shape[0] - 1
+
+    _metric = np.choose(tp_fvt, sorted_da_ids.values)
+    return pd.Series(_metric)
