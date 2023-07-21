01adfb23806779a95796f2aaad4a239295a50811
Swainlab <peter.swain@ed.ac.uk>
2023-07-21 12:56:54 +0100

    added and verified validate_lineage, now used by picker

--- src/postprocessor/core/reshapers/picker.py
+++ src/postprocessor/core/reshapers/picker.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 from agora.abc import ParametersABC
 from agora.io.cells import Cells
-from agora.utils.indexing_new import validate_association
+from agora.utils.indexing import validate_association
+from agora.utils.indexing_new import validate_lineage
 from agora.utils.cast import _str_to_int
 from agora.utils.kymograph import drop_mother_label
 from postprocessor.core.lineageprocess import LineageProcess
@@ -59,7 +60,25 @@ class Picker(LineageProcess):
         """
         cells_present = drop_mother_label(signal.index)
         mothers_daughters = self.get_lineage_information(signal)
+        _, valid_indices = validate_lineage(
+            mothers_daughters, cells_present, how
+        )
+        return signal.index[valid_indices]
+
+    def pick_by_lineage_original(
+        self,
+        signal: pd.DataFrame,
+        how: str,
+        mothers_daughters: t.Optional[np.ndarray] = None,
+    ) -> pd.MultiIndex:
+        """
+        Return rows of a signal corresponding to either mothers, daughters,
+        or mother-daughter pairs using lineage information.
+        """
+        cells_present = drop_mother_label(signal.index)
+        mothers_daughters = self.get_lineage_information(signal)
         #: might be better if match_column defined as a string to make everything one line
+        breakpoint()
         if how == "mothers":
             _, valid_indices = validate_association(
                 mothers_daughters, cells_present, match_column=0
