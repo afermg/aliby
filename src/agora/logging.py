0252277b1f0194ed2dd7e6c54405afb0511c2741
Peter Swain <peter.swain@ed.ac.uk>
2023-10-27 16:12:19 +0100

    docs to logging

--- src/agora/logging.py
+++ src/agora/logging.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env jupyter
-"""
-Add general logging functions and decorators
-"""
-
 import logging
 from time import perf_counter
 
 
 def timer(func):
-    # Log duration of a function into aliby logfile
+    """Log duration of a function into the aliby log file."""
+
     def wrap_func(*args, **kwargs):
         t1 = perf_counter()
         result = func(*args, **kwargs)
