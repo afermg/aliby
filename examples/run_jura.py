33144f6f331df7502a19fc30c8d758693552dd9a
pswain <peter.swain@ed.ac.uk>
2024-05-07 12:50:40 +0100

    change: replaced examples with simple scripts

---
+++ examples/run_jura.py
@@ -0,0 +1,16 @@
+from aliby.pipeline import PipelineParameters, Pipeline
+
+params = PipelineParameters.default(
+    general={
+        "expt_id": 2172,
+        "distributed": 0,
+        "directory": ".",
+        "host": "staffa.bio.ed.ac.uk",
+        "username": pass,
+        "password": pass,
+    }
+)
+# specify OMERO_channels if the channels on OMERO have a different order from the logfiles
+p = Pipeline(params)
+
+p.run()
---
+++ examples/run_local.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from aliby.pipeline import Pipeline, PipelineParameters
+from postprocessor.grouper import Grouper
+
+omid = "25681_2022_04_30_flavin_htb2_glucose_10mgpL_01_00"
+
+h5dir = "/Users/pswain/wip/aliby_output/"
+omero_dir = "/Users/pswain/wip/aliby_input/"
+
+# setup and run pipeline
+params = PipelineParameters.default(
+    general={
+        "expt_id": omero_dir + omid,
+        "distributed": 2,
+        "directory": h5dir,
+        # specify position to segment
+        "filter": "fy4_007",
+        # specify final time point
+        # "tps": 4,
+    },
+)
+
+
+# initialise and run pipeline
+p = Pipeline(params, OMERO_channels=["Brightfield", "Flavin"])
+p.run()
