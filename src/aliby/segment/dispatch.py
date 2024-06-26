b55dc9b5d0b7123e8a021e575189060cad1030d2
Alan Munoz <afer.mg@gmail.com>
2024-06-26 18:54:31 -0400

    [WIP]change(segment): update seg dispatcher

--- src/aliby/segment/dispatch.py
+++ src/aliby/segment/dispatch.py
@@ -5,43 +5,47 @@ See all the available models at
 https://cellpose.readthedocs.io/en/latest/models.html#full-built-in-models
 """
 
-from agora.abc import StepABC
+import numpy as np
 
 
 def dispatch_segmenter(kind, **kwargs) -> callable:
-    if kind == "baby":
-        import os
-        import logging
-        import tensorflow as tf
-        from aliby.baby_client import BabyParameters, BabyRunner
+    match kind:
+        case "baby":
+            import logging
+            import os
 
-        # stop warnings from TensorFlow
-        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-        logging.getLogger("tensorflow").setLevel(logging.ERROR)
+            import tensorflow as tf
+            from aliby.baby_client import BabyParameters, BabyRunner
 
-        initialise_tensorflow()
-        segmenter_cls, segmenter_params = BabyRunner, BabyParameters
-        segment = segmenter_cls.from_tiler(
-            segmenter_params.from_dict(config["segmenter"]),
-            tiler=kwargs["tiler"],
-        )
-    else:  # One of the cellpose models
-        # cellpose does without all the ABC stuff
-        # It returns a function to segment
-        from cellpose.models import CellposeModel
+            # stop warnings from TensorFlow
+            os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+            logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
-        model = kind
-        argname = "model_type"
-        # use custom models if fullpath is provided
-        if model.startswith("/"):
-            argname = "pretrained_model"
-        model = CellposeModel(**{argname: model})
+            initialise_tensorflow()
+            segmenter_cls, segmenter_params = BabyRunner, BabyParameters
+            # TODO fix this
+            segment = segmenter_cls.from_tiler(
+                segmenter_params.from_dict(config["segmenter"]),
+                tiler=kwargs["tiler"],
+            )
+        case _:  # One of the cellpose models
+            # cellpose does without all the ABC stuff
+            # It returns a function to segment
+            from cellpose.models import CellposeModel
 
-        # ensure it returns only masks
-        def segment(*args):
-            return model.eval(*args, **kwargs)[0]
+            model = kind
+            argname = "model_type"
+            # use custom models if fullpath is provided
+            if model.startswith("/"):
+                argname = "pretrained_model"
+            model = CellposeModel(**{argname: model})
 
-        return segment
+            # ensure it returns only masks
+            # TODO generalise so it does not assume a 1-tile file
+            def segment(*args) -> list[np.ndarray]:
+                return [model.eval(*args, **kwargs)[0]]
+
+            return segment
 
     return segment
 
@@ -54,6 +58,4 @@ def initialise_tensorflow(version=2):
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.experimental.list_logical_devices("GPU")
-            print(
-                len(gpus), "physical GPUs,", len(logical_gpus), "logical GPUs"
-            )
+            print(len(gpus), "physical GPUs,", len(logical_gpus), "logical GPUs")
