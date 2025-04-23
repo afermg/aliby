a5156a8962ccd6af0c0a0b48a305d017f1d5732c
Alan Munoz <afer.mg@gmail.com>
2025-04-23 16:12:18 -0400

    test(extraction): add monomeasurement test script

---
+++ tests/extraction/test_measure_mono.py
@@ -0,0 +1,26 @@
+import dask.array as da
+
+from extraction.extract import extract_tree, process_tree_masks
+
+masks = [da.zeros((6, 100, 100), dtype=int)]
+ones = da.ones((15, 15))
+for i in range(6):
+    masks[0][i, 15 * i : 15 * (i + 1), 15 * i : 15 * (i + 1)] = i + 1
+masks[0] = masks[0].max(axis=0)
+rng = da.random.default_rng(1)
+pixels = rng.standard_normal(size=(3, 5, 100, 100)).compute()
+tree = {
+    None: {
+        "max": [
+            "area",
+            "centroid_x",
+            "centroid_y",
+        ]
+    },
+    1: {
+        "max": [
+            "max2p5pc",
+        ]
+    },
+}
+result = process_tree_masks(tree, masks, pixels, extract_tree)
