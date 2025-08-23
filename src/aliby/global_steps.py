39d0521439efb4f3ab1157d2464a78a97633922f
Alán F. Muñoz <afer.mg@gmail.com>
2025-08-23 17:20:18 -0400

    fix(global_steps): match nahual 0.0.5 interface

--- src/aliby/global_steps.py
+++ src/aliby/global_steps.py
@@ -4,28 +4,22 @@ Define the processes that take entire time series (global) data as inputs.
 The most common example is tracking methods, which require object object masks alongside images.
 """
 
+from functools import partial
 from typing import Callable
 
-import nahual.client.trackastra as tr
 import numpy
 import pyarrow
+from nahual.process import dispatch_setup_process
 
 
-def nahual_trackastra_load_model(
-    parameters: dict[str, str], address: str
-) -> dict[str, str]:
-    model_info = tr.load_model(parameters, address=address)
-    return model_info
-
-
-def nahual_trackastra_process_data(
-    input_data: numpy.ndarray, address: str
+def nahual_trackastra_process_format(
+    input_data: numpy.ndarray, address: str, process: Callable
 ) -> pyarrow.Table:
     """
     Expand the list  of data and receive the response, finally convert the resultant
     dictionaries into a pyarrow table.
     """
-    tracking = tr.process_data(data=input_data, address=address)
+    tracking = process(data=input_data, address=address)
 
     tracks = pyarrow.Table.from_pydict(tracking)
 
@@ -37,6 +31,7 @@ def dispatch_global_step(step_name: str) -> tuple[Callable, Callable]:
     Return the functions to load and process data.
     """
     if step_name == "nahual_trackastra":
-        return nahual_trackastra_load_model, nahual_trackastra_process_data
-    else:
-        raise Exception(f"Step name {step_name} not found.")
+        setup, process = dispatch_setup_process("trackastra")
+        process_format = partial(nahual_trackastra_process_format, process=process)
+
+    return setup, process_format
