470e704e0a49f0cf151583b9265aba1eaf1edfee
Alán F. Muñoz <amunozgo@broadinstitute.org>
2025-11-20 19:59:21 -0500

    [WIP]feat: add working version of multiprocessing

--- src/aliby/pipe.py
+++ src/aliby/pipe.py
@@ -173,7 +173,7 @@ def pipeline_step(
         # Save state
         steps_to_write = pipeline.get("save", [])
         save_interval = pipeline.get("save_interval", 0)
-        if len(steps_to_write) and not (tp % save_interval):
+        if len(steps_to_write) and (tp == 0 or not (tp % save_interval)):
             print(f"Saving {step_name} to {steps_dir}")
             if step_name in steps_to_write:
                 write_fn = dispatch_write_fn(step_name)
--- tests/test_basic_pipeline_monozarr.py
+++ tests/test_basic_pipeline_monozarr.py
@@ -3,6 +3,7 @@
 
 from itertools import combinations, product
 from pathlib import Path
+from time import perf_counter
 
 from aliby.io.dataset import dispatch_dataset
 from aliby.pipe import run_pipeline_and_post
@@ -20,107 +21,128 @@ def _create_extract_multich_tree(channels: list[int]) -> dict:
     }
 
 
-fpath = "/work/datasets/jump_toy/zstd.zarr"
+# fpath = "/work/datasets/jump_toy_compress/zstd.zarr/"
+fpath = "/work/datasets/jump_target2_subset_BR00121438/zstd.zarr/"
 dset = dispatch_dataset(fpath, is_monozarr=True)
 image_paths = dset.get_position_ids()
 
-input_path = list(image_paths.values())[0]
-fluo_base_config = {
-    "input_path": input_path,
-    "capture_order": "CYX",
-    "ntps": 1,
-    "segmentation_channel": {"nuclei": 1, "cell": 4},
-    # "gstep_params": {},
-}
-fl_channels = range(1, 3)
-
-segmentation_channel: dict[str, int] = fluo_base_config["segmentation_channel"]
-seg_params = {
-    f"segment_{obj}": dict(
-        segmenter_kwargs=dict(
-            kind="cellpose",
-        ),
-        img_channel=ch_id,
-    )
-    for i, (obj, ch_id) in enumerate(fluo_base_config["segmentation_channel"].items())
-}
-
-extract_base = dict(
-    channels=fl_channels,
-    tree={
-        **{
-            i: {
-                "max": [
-                    # "radial_zernikes",
-                    "intensity",
-                    "sizeshape",
-                    # "ferret",
-                    # "texture",
-                    # "radial_distribution",
-                    # "zernike",
-                    # "granularity", # Too time-consuming, deactivated for now
-                ]
-            }
-            for i in fl_channels
-        },
-    },
-)
-
-ext_params = {
-    f"extract{name}_{obj}": var
-    for (name, var), obj in product(
-        (
-            ("", extract_base),
-            # ("multi", extract_multich_tree),
-        ),
-        segmentation_channel,
-    )
-    if len(var)
-}
+# %%
+input_paths = list(image_paths.values())
 
 
-base_pipeline = {
-    "io": {**fluo_base_config},
-    "nchannels": 2,
-    "fl_channels": fl_channels,
-    "extract_multich_tree": _create_extract_multich_tree(range(1, 3)),
-    "steps": dict(
-        tile=dict(
-            image_kwargs=dict(
-                source=input_path,
-                # regex=regex,
-                capture_order=fluo_base_config["capture_order"],
-                # dimorder=fluo_base_config["dimorder"],
+def process_input_path(input_path: str):
+    fluo_base_config = {
+        "input_path": input_path,
+        "capture_order": "CYX",
+        "ntps": 1,
+        "segmentation_channel": {"nuclei": 1, "cell": 4},
+        # "gstep_params": {},
+    }
+    fl_channels = range(1, 3)
+
+    segmentation_channel: dict[str, int] = fluo_base_config["segmentation_channel"]
+    seg_params = {
+        f"segment_{obj}": dict(
+            segmenter_kwargs=dict(
+                kind="cellpose",
             ),
-            tile_size=None,
-            ref_channel=0,
-            ref_z=0,
-            calculate_drift=False,
-        ),
-        **seg_params,
-        **ext_params,
-    ),
-    "passed_data": dict(
-        **{
-            f"extract_{obj}": [
-                ("masks", f"segment_{obj}"),
-                ("pixels", "tile"),
-            ]
-            for obj in fluo_base_config["segmentation_channel"]
-        },
-    ),
-    "passed_methods": {
-        f"segment_{obj}": ("tile", "get_tp_data", "img_channel")
-        for obj in segmentation_channel
-    },
-}
+            img_channel=ch_id,
+        )
+        for i, (obj, ch_id) in enumerate(
+            fluo_base_config["segmentation_channel"].items()
+        )
+    }
 
-output_path = Path("output")
-capture_order: str = base_pipeline["io"]["capture_order"]
-result, _ = run_pipeline_and_post(
-    pipeline=base_pipeline,
-    img_source=input_path,
-    output_path=output_path,
-    fov=input_path.name[1:],
-    overwrite=True,
-)
+    extract_base = dict(
+        channels=fl_channels,
+        tree={
+            **{
+                i: {
+                    "max": [
+                        "radial_zernikes",
+                        "intensity",
+                        "sizeshape",
+                        "ferret",
+                        "texture",
+                        "radial_distribution",
+                        "zernike",
+                        # "granularity", # Too time-consuming, deactivated for now
+                    ]
+                }
+                for i in fl_channels
+            },
+        },
+    )
+
+    ext_params = {
+        f"extract{name}_{obj}": var
+        for (name, var), obj in product(
+            (
+                ("", extract_base),
+                # ("multi", extract_multich_tree),
+            ),
+            segmentation_channel,
+        )
+        if len(var)
+    }
+
+    base_pipeline = {
+        "io": {**fluo_base_config},
+        "nchannels": 2,
+        "fl_channels": fl_channels,
+        "extract_multich_tree": _create_extract_multich_tree(range(1, 3)),
+        "steps": dict(
+            tile=dict(
+                image_kwargs=dict(
+                    source=input_path,
+                    # regex=regex,
+                    capture_order=fluo_base_config["capture_order"],
+                    # dimorder=fluo_base_config["dimorder"],
+                ),
+                tile_size=None,
+                ref_channel=0,
+                ref_z=0,
+                calculate_drift=False,
+            ),
+            **seg_params,
+            **ext_params,
+        ),
+        "passed_data": dict(
+            **{
+                f"extract_{obj}": [
+                    ("masks", f"segment_{obj}"),
+                    ("pixels", "tile"),
+                ]
+                for obj in fluo_base_config["segmentation_channel"]
+            },
+        ),
+        "passed_methods": {
+            f"segment_{obj}": ("tile", "get_tp_data", "img_channel")
+            for obj in segmentation_channel
+        },
+    }
+
+    expt_name = str(input_path).split("/")[5]
+    output_path = Path("/work/datasets/") / expt_name / input_path.path
+
+    # t0 = perf_counter()
+    result, _ = run_pipeline_and_post(
+        pipeline=base_pipeline,
+        img_source=input_path,
+        output_path=output_path,
+        fov=input_path.path,
+        overwrite=True,
+    )
+    # print(f"Data {i} took {perf_counter() - t0} seconds")
+
+
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+
+with ProcessPoolExecutor() as p:
+    result = list(
+        p.map(
+            process_input_path,
+            input_paths,
+        )
+    )
